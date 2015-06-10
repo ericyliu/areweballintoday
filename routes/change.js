2fc80ad46d93979261a6c3a3ce6a127cc515c315
Eric Liu <eric.yuhui.liu@gmail.com>
2015-06-10 17:33:21 -0400

    added a way to change status and time

--- routes/change.js
+++ routes/change.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ballin = require('../models/ballin.js');
+var ballin = require('../models/ballin');
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log(req.query.status);
+  ballin.status = req.query.status;
+  ballin.time = req.query.time;
   res.render('index', { title: 'Are We Ballin Today?', status: ballin.status, time: ballin.time });
 });
 
